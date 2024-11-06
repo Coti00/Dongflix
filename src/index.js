72e6ecb3115b67a60ef1c774d0c1da32077e426b
Coti00 <idjda1130@naver.com>
2024-11-07 00:39:49 +0900

    v.0.1

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename="/Netflix">
+    <BrowserRouter basename="/Netflix_Clone">
       <App />
     </BrowserRouter>
 );
